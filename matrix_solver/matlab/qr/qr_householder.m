4f515bd718e4d4df3868657f3f2d59d79d558b91
ziv780316 <north780316@gmail.com>
2018-11-15 00:46:45 +0800

    modified QR decomposition to QRP

--- matrix_solver/matlab/qr/qr_householder.m
+++ matrix_solver/matlab/qr/qr_householder.m
@@ -9,19 +9,45 @@
 % Hn * ... * H1 * A = R --> n = col - 1
 % Q = H1' * ... * Hn'
 
-function [Q, R, rank] = qr_householder( A, remove_redundant, debug )
+function [Q, R, P, rank] = qr_householder( A, remove_redundant, debug )
 
 	mat_size = size(A);
 	nr = mat_size(1);
 	nc = mat_size(2);
-	rank = nc;
 	
+	min_norm = 1e-10;
+	P = eye(nc);
 	Ai = A;
 	for i = 1:1:(nc - 1)
-		vi = Ai(:, i);
-		if i > 1
-			vi(1:i-1) = 0;
+		v_norm = 0;
+		pivot = i;
+		while v_norm < min_norm
+			ai = Ai(:,pivot);
+			vi = ai;
+			if i > 1
+				vi(1:i-1) = 0;
+			end
+
+			v_norm = norm(vi, 2);
+			% Ai is linear depend on other vector is v_norm -> 0
+			if v_norm < min_norm
+				pivot = pivot + 1;
+			end
+			if pivot > nc
+				break;
+			end
 		end
+
+		if pivot ~= i
+			tmp = P(:,i);
+			P(:,i) = P(:,pivot);
+			P(:,pivot) = tmp;
+
+			tmp = Ai(:,i);
+			Ai(:,i) = Ai(:,pivot);
+			Ai(:,pivot) = tmp;
+		end
+
 		wi = zeros(nr, 1);
 		wi(i) = norm(vi, 2);
 		ui = (vi - wi) / norm(vi - wi, 2);
@@ -33,10 +59,15 @@ function [Q, R, rank] = qr_householder( A, remove_redundant, debug )
 		end
 
 		if debug
+			fprintf('Ai:\n');
 			disp(Ai);
+			fprintf('vi:\n');
 			disp(vi);
+			fprintf('wi:\n');
 			disp(wi);
+			fprintf('ui:\n');
 			disp(ui);
+			fprintf('Hi:\n');
 			disp(Hi);
 		end
 
@@ -45,12 +76,22 @@ function [Q, R, rank] = qr_householder( A, remove_redundant, debug )
 
 	R = Ai;
 
+	% count rank
+	rank = 0;
+	for i = 1:1:nc
+		if abs(R(i,i)) > 1e-10
+			rank = rank + 1;
+		end
+	end
+
 	if remove_redundant
+		k = 1;
 		for i = 1:1:nc
-			if abs(R(i,i)) < 1e-12
+			if abs(R(k,i)) < 1e-10
 				fprintf('delete redundant column %d\n', i);
-				R(:,i) = [];
+				R(k,:) = [];
 			end
+			k = k + 1;
 		end
 	end
 
--- matrix_solver/matlab/qr/qr_linsolve.m
+++ matrix_solver/matlab/qr/qr_linsolve.m
@@ -1,20 +1,38 @@
 
-function x = qr_linsolve( A, b )
+function x = qr_linsolve( A, b, debug )
 
-	size_a = size(A);
-	na = size_a(2);
+	size_A = size(A);
+	nrow = size_A(1);
+	ncol = size_A(2);
 
-	[Q R]=qr_householder(A', 1, 0);
+	[Q,R,P,rank]=qr_householder(A,0,0);
 
-	size_r = size(R);
-	nr = size_r(2);
+	R11 = R(1:rank,1:rank);
+	y = Q'*b;
+	y1 = y(1:rank);
+	x1 = R11\y1;
+	x = zeros(ncol,1);
+	x(1:rank) = x1;
+	x = P*x;
 
-	if nr < na
-		b(nr+1:end) = [];
+	if debug
+		fprintf('Q:\n');
+		disp(Q);
+		fprintf('R11:\n');
+		disp(R11);
+		fprintf('P:\n');
+		disp(P);
+		fprintf('y1:\n');
+		disp(y1);
+		fprintf('x1:\n');
+		disp(x1);
+		fprintf('x:\n');
+		disp(x);
+		fprintf('A*x:\n');
+		disp(A*x);
 	end
 
-	C = R' * R;
-	y = C\b;
-	x = Q * R * y;
-
+	%C = R' * R;
+	%y = C\b;
+	%x = Q * R * y;
 end
