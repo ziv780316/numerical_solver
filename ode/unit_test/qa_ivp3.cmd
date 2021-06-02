99d764252a1b3a9795b0fa5668959ea4051a0a3c
ziv <north780316@gamil.com>
2021-06-02 14:59:57 +0800

    [Description] modify
    [Commit Files]
    ode/adams_bashforth.c
    ode/adams_moulton.c
    ode/bdf.c
    ode/diff.c
    ode/unit_test/ab_ivp1.gnuplot
    ode/unit_test/ab_ivp3.gnuplot
    ode/unit_test/ab_ivp4.gnuplot
    ode/unit_test/am_ivp1.gnuplot
    ode/unit_test/am_ivp3.gnuplot
    ode/unit_test/bdf_ivp1.gnuplot
    ode/unit_test/bdf_ivp4.gnuplot
    ode/unit_test/error_propagate.gnuplot
    ode/unit_test/gen_f.sh
    ode/unit_test/local.gnuplot
    ode/unit_test/qa_ivp1.cmd
    ode/unit_test/qa_ivp3.cmd
    ode/unit_test/qa_ivp4.cmd
    ode/unit_test/rk_ivp1.gnuplot
    ode/unit_test/rk_ivp4.gnuplot
    ode/unit_test/stiff_ivp1.gnuplot
    
    -------------------------------------------

--- ode/unit_test/qa_ivp3.cmd
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-debug="-d"
-predictor="-p"
-
-# stability test of AB
-../bin/ode_solver -m ab -o 1 -t 0.19 ${predictor} > output_ivp3_ab1_t0p19
-../bin/ode_solver -m ab -o 1 -t 0.21 ${predictor} > output_ivp3_ab1_t0p21
-
