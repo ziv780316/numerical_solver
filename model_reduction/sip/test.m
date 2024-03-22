c5f092e2ff111827796ca9e23270ad0725ae2e1b
ziv <north780316@gamil.com>
2024-03-22 21:00:43 +0800

    [Description] update
    [Commit Files]
    model_reduction/sip/test.m
    
    -------------------------------------------

--- model_reduction/sip/test.m
+++ model_reduction/sip/test.m
@@ -1,21 +1,30 @@
 %
 format long e;
 
-% r1 1 2 1
-% c1 2 0 1
-% r2 2 3 1
-% c2 3 0 1
-% r3 3 4 1
+% r12 1 2 1
+% c11 2 0 c11
+% r22 2 0 r22
+% r23 2 3 1
+% c22 3 0 c22
+% r34 3 4 1
+% c23 2 3 c23
+
+g22 = 1;
 G  = [...
 1 -1 0 0;...
--1 2 -1 0;...
+-1 2+g22 -1 0;...
 0 -1 2 -1;...
 0 0 -1 1;...
 ];
+
+c12 = 0;
+c22 = 1;
+c23 = 0;
+c33 = 2;
 C  = [...
-0 0 0 0;...
-0 2 -1 0;...
-0 -1 2 0;...
+c12 -c12 0 0;...
+-c12 c22+c23+c12 -c23 0;...
+0 -c23 c33+c23 0;...
 0 0 0 0;...
 ];
 
@@ -62,12 +71,34 @@ C_ext
 fprintf('C_neg=\n');
 C_neg
 fprintf('C_all=\n');
-C_ext + C_neg
+C_all = C_ext + C_neg
 
 M = [...
 -(Gii \ Gic);...
 eye(2,2);...
 ];
 
-M'*Gp*M
-M'*Cp*M
+fprintf('M''*Gp*M=\n');
+G_sip = M'*Gp*M
+fprintf('M''*Cp*M=\n');
+C_sip = M'*Cp*M
+
+fprintf('|G_sip-G_ext|=%.10e\n', max(max(G_sip-G_ext)));
+fprintf('|C_sip-C_all|=%.10e\n', max(max(C_sip-C_all)));
+
+c_cc = -C_sip(1,2);
+c_lump1 = C_sip(1,1)-c_cc;
+c_lump2 = C_sip(2,2)-c_cc;
+fprintf('c_cc=%.10e\n', c_cc);
+fprintf('c_lump1=%.10e\n', c_lump1);
+fprintf('c_lump2=%.10e\n', c_lump2);
+fprintf('c_lump1_qreduce=%.10e\n', C_sip(1,1));
+fprintf('c_lump2_qreduce=%.10e\n', C_sip(2,2));
+
+g_gg = -G_sip(1,2);
+g_lump1 = G_sip(1,1)-g_gg;
+g_lump2 = G_sip(2,2)-g_gg;
+fprintf('g_gg=%.10e\n', g_gg);
+fprintf('g_lump1=%.10e\n', g_lump1);
+fprintf('g_lump2=%.10e\n', g_lump2);
+
